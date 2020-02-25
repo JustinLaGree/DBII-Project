5577bbebdcf4408f86e031d134c17a9ee76eba5e
Ryan Allen <rrallen2002@gmail.com>
2020-02-25 12:11:12 -0500

    Replaced w/ TA's code and added dummy rows

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -1,150 +1,239 @@
-DROP TABLE IF EXISTS participates_in;
-DROP TABLE IF EXISTS registered_for;
-DROP TABLE IF EXISTS assigned_for;
+/*
+ Navicat MySQL Data Transfer
 
-DROP TABLE IF EXISTS students;
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 100137
+ Source Host           : localhost:3306
+ Source Schema         : db2
 
-DROP TABLE IF EXISTS admins;
-DROP TABLE IF EXISTS parents;
+ Target Server Type    : MySQL
+ Target Server Version : 100137
+ File Encoding         : 65001
 
-DROP TABLE IF EXISTS users;
+ Date: 2/12/2020 22:55:30
+*/
 
-DROP TABLE IF EXISTS meetings;
-DROP TABLE IF EXISTS groups;
-DROP TABLE IF EXISTS study_materials;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS year_info;
+
+DROP TABLE IF EXISTS `enroll`;
+DROP TABLE IF EXISTS `enroll2`;
+DROP TABLE IF EXISTS `assign`;
+DROP TABLE IF EXISTS `mentors`;
+DROP TABLE IF EXISTS `mentees`;
+DROP TABLE IF EXISTS `material`;
+DROP TABLE IF EXISTS `meetings`;
+DROP TABLE IF EXISTS `time_slot`;
+DROP TABLE IF EXISTS `groups`;
+DROP TABLE IF EXISTS `admins`;
+DROP TABLE IF EXISTS `students`;
+DROP TABLE IF EXISTS `parents`;
+DROP TABLE IF EXISTS `users`;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  firstName varchar(255) NOT NULL,
-  lastName varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  phone varchar(255) DEFAULT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (id) USING BTREE,
-  UNIQUE KEY email (email)
-);
-
--- TODO: Add trigger to check if id exists in the other of two types of user tables
-
--- ----------------------------
--- Table structure for admins
--- ----------------------------
-
-CREATE TABLE admins (
-  id int(11) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT admin_user FOREIGN KEY (id) REFERENCES Users (id) ON DELETE CASCADE
-);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for parents
 -- ----------------------------
 
-CREATE TABLE parents (
-  id int(11) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT parent_user FOREIGN KEY (id) REFERENCES Users (id) ON DELETE CASCADE
-);
+CREATE TABLE `parents` (
+  `parent_id` int(11) NOT NULL,
+  PRIMARY KEY (`parent_id`),
+  CONSTRAINT `parent_user` FOREIGN KEY (`parent_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for students
 -- ----------------------------
 
-CREATE TABLE students (
-  id int(11) NOT NULL,
-  grade int(11) NOT NULL,
-  parent_id int(11) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT student_user FOREIGN KEY (id) REFERENCES Users (id) ON DELETE CASCADE,
-  CONSTRAINT student_parent FOREIGN KEY (parent_id) REFERENCES Parents (id) ON DELETE CASCADE
-);
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `parent_id` int(11) NOT NULL,
+  PRIMARY KEY (`student_id`),
+  KEY `student_parent` (`parent_id`),
+  CONSTRAINT `student_user` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `student_parent` FOREIGN KEY (`parent_id`) REFERENCES `parents` (`parent_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for admins
+-- ----------------------------
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  CONSTRAINT `admins_user` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- ----------------------------
 -- Table structure for groups
 -- ----------------------------
 
-CREATE TABLE groups (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  description varchar(255) DEFAULT NULL,
-  mentor_grade_req int(11) NOT NULL,
-  mentee_grade_req int(11) NOT NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `mentor_grade_req` int(11) NOT NULL,
+  `mentee_grade_req` int(11) NOT NULL,
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- ----------------------------
--- Table structure for registered_for
+-- Table structure for time_slot
 -- ----------------------------
 
-CREATE TABLE registered_for (
-  student_id int(11) NOT NULL,
-  group_id int(11) NOT NULL,
-  mentor_or_mentee bit(1) NOT NULL,
-  PRIMARY KEY (student_id, group_id)
-);
+CREATE TABLE `time_slot` (
+  `time_slot_id` int(11) NOT NULL AUTO_INCREMENT,
+  `day_of_the_week` varchar(255) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  PRIMARY KEY (`time_slot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for meetings
 -- ----------------------------
 
-CREATE TABLE meetings (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  date date DEFAULT NULL,
-  time int(11) NOT NULL,
-  capacity int(11) NOT NULL,
-  url varchar(255) NOT NULL,
-  announcement varchar(255) DEFAULT NULL,
-  group_id int(11) DEFAULT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT meeting_group FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE
-);
+CREATE TABLE `meetings` (
+  `meet_id` int(11) NOT NULL AUTO_INCREMENT,
+  `meet_name` varchar(255) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time_slot_id` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `announcement` varchar(255) DEFAULT NULL,
+  `group_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`meet_id`),
+  KEY `meeting_group` (`group_id`),
+  KEY `meeting_time_slot` (`time_slot_id`),
+  CONSTRAINT `meeting_group` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE CASCADE,
+  CONSTRAINT `meeting_time_slot` FOREIGN KEY (`time_slot_id`) REFERENCES `time_slot` (`time_slot_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for participates_in
+-- Table structure for material
 -- ----------------------------
 
-CREATE TABLE participates_in (
-  student_id int(11) NOT NULL,
-  meeting_id int(11) NOT NULL,
-  PRIMARY KEY (student_id, meeting_id)
-);
+CREATE TABLE `material` (
+  `material_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `assigned_date` date NOT NULL,
+  `notes` text,
+  PRIMARY KEY (`material_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for study_materials
+-- Table structure for mentees
 -- ----------------------------
 
-CREATE TABLE study_materials (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL,
-  author varchar(255) DEFAULT NULL,
-  type varchar(255) NOT NULL,
-  url varchar(255) DEFAULT NULL,
-  assigned_date date NOT NULL,
-  notes text,
-  PRIMARY KEY (id)
-);
+CREATE TABLE `mentees` (
+  `mentee_id` int(11) NOT NULL,
+  PRIMARY KEY (`mentee_id`),
+  CONSTRAINT `mentee_student` FOREIGN KEY (`mentee_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for assigned_for
+-- Table structure for mentors
 -- ----------------------------
 
-CREATE TABLE assigned_for (
-  study_materials_id int(11) NOT NULL,
-  meeting_id int(11) NOT NULL,
-  PRIMARY KEY (study_materials_id, meeting_id)
-);
+CREATE TABLE `mentors` (
+  `mentor_id` int(11) NOT NULL,
+  PRIMARY KEY (`mentor_id`),
+  CONSTRAINT `mentor_student` FOREIGN KEY (`mentor_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for year_info
+-- Table structure for enroll
 -- ----------------------------
 
-CREATE TABLE year_info (
-  start_date date NOT NULL,
-  end_date date NOT NULL
-);
+CREATE TABLE `enroll` (
+  `meet_id` int(11) NOT NULL,
+  `mentee_id` int(11) NOT NULL,
+  PRIMARY KEY (`meet_id`,`mentee_id`),
+  KEY `enroll_mentee` (`mentee_id`),
+  CONSTRAINT `enroll_mentee` FOREIGN KEY (`mentee_id`) REFERENCES `mentees` (`mentee_id`) ON DELETE CASCADE,
+  CONSTRAINT `enroll_meetings` FOREIGN KEY (`meet_id`) REFERENCES `meetings` (`meet_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for enroll2
+-- ----------------------------
+
+CREATE TABLE `enroll2` (
+  `meet_id` int(11) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  PRIMARY KEY (`meet_id`,`mentor_id`),
+  KEY `enroll2_mentor` (`mentor_id`),
+  CONSTRAINT `enroll2_mentor` FOREIGN KEY (`mentor_id`) REFERENCES `mentors` (`mentor_id`) ON DELETE CASCADE,
+  CONSTRAINT `enroll2_meetings` FOREIGN KEY (`meet_id`) REFERENCES `meetings` (`meet_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for assign
+-- ----------------------------
+
+CREATE TABLE `assign` (
+  `meet_id` int(11) NOT NULL,
+  `material_id` int(11) NOT NULL,
+  PRIMARY KEY (`meet_id`,`material_id`),
+  KEY `assign_material` (`material_id`),
+  KEY `assign_meetings` (`meet_id`),
+  CONSTRAINT `assign_material` FOREIGN KEY (`material_id`) REFERENCES `material` (`material_id`) ON DELETE CASCADE,
+  CONSTRAINT `assign_meetings` FOREIGN KEY (`meet_id`) REFERENCES `meetings` (`meet_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+INSERT INTO users 
+VALUES 
+(1, 'HopeJohnson@gmail.com', 'chalkboard', 'Hope Johnson', '808-777-2222'),
+(2, 'JohnSmith@gmail.com', 'password123', 'John Smith', '802-800-8450'),
+(3, 'WillSmith@gmail.com', 'table', 'Will Smith', '802-800-8999'),
+(4, 'BenBob@gmail.com', 'mechanicalpencil', 'Ben Bob', '805-800-1234'),
+(5, 'JimBob@gmail.com', 'sweathshirt', 'Jim Bob', '807-800-8543'),
+(6, 'LeeMilk@gmail.com', 'laptop', 'Lee Milk', '812-800-7777'),
+(7, 'LannySmith@gmail.com', 'chair', 'Lanny Smith', '833-800-8876'),
+(8, 'SarahWelton@gmail.com', 'tilefloor', 'Sarah Welton', '800-800-8111'),
+(9, 'MileyWelton@gmail.com', 'waterbottle', 'Miley Welton', '800-800-8222'),
+(10, 'DannyJohnson@gmail.com', 'whiteboard', 'Danny Johnson', '808-800-9999');
+
+INSERT INTO admins 
+VALUES 
+(6);
+
+INSERT INTO parents
+VALUES 
+(1), 
+(2), 
+(4), 
+(8);
+
+INSERT INTO students
+VALUES 
+(3, 7, 2),
+(5, 9, 4),
+(7, 11, 2),
+(9, 9, 8),
+(10, 6, 1);
