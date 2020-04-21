248501fede65f6bf6c8cfa80be8691813f684c14
unknown <justinlagree@live.com>
2020-04-21 11:21:47 -0400

    Get initial list of meetings for admin.

--- Phase3/Java/db2/app/src/main/java/com/example/db2/MeetingsAdminActivity.java
+++ Phase3/Java/db2/app/src/main/java/com/example/db2/MeetingsAdminActivity.java
@@ -1,12 +1,20 @@
 package com.example.db2;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
+import com.example.db2.helpers.QueryExecution;
 import com.example.db2.helpers.UserSession;
+import com.example.db2.models.Meeting;
+
+import java.util.ArrayList;
 
 public class MeetingsAdminActivity extends BaseLogoutActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
@@ -15,5 +23,10 @@ public class MeetingsAdminActivity extends BaseLogoutActivity {
         final TextView welcome_text = findViewById(R.id.welcome_text);
         String welcomeStr = String.format("Welcome, %s!", UserSession.getInstance().name);
         welcome_text.setText(welcomeStr);
+
+        String query = "SELECT * FROM meetings ORDER BY date ASC";
+        QueryExecution.executeQuery(query);
+
+        ArrayList<Meeting> meetings = QueryExecution.getResponse(Meeting.class);
     }
 }
---
+++ Phase3/Java/db2/app/src/main/java/com/example/db2/models/Meeting.java
@@ -0,0 +1,13 @@
+package com.example.db2.models;
+
+import java.util.Date;
+
+public class Meeting {
+    public int meet_id;
+    public String meet_name;
+    public Date date;
+    public int time_slot_id;
+    public int capacity;
+    public String announcement;
+    public int group_id;
+}
