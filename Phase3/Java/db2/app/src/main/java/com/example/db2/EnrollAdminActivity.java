4fec6514ab765927bdc5e4a512f6f6f83df0dcc8
unknown <justinlagree@live.com>
2020-04-24 17:34:39 -0400

    Implement full functionality for admin enroll.

--- Phase3/Java/db2/app/src/main/java/com/example/db2/EnrollAdminActivity.java
+++ Phase3/Java/db2/app/src/main/java/com/example/db2/EnrollAdminActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.db2.adapters.MeetingAdminAdapter;
@@ -23,6 +24,8 @@ import com.example.db2.models.Meeting;
 import com.example.db2.models.Student;
 import com.example.db2.models.TimeSlot;
 import com.example.db2.models.User;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -55,16 +58,34 @@ public class EnrollAdminActivity extends BaseLogoutBackActivity {
 
         buildMeetingCard();
 
+        TextView menteeEnrolledText = findViewById(R.id.mentee_enrolled_textView);
+        TextView menteeAvailableText = findViewById(R.id.mentee_available_textView);
+        TextView mentorEnrolledText = findViewById(R.id.mentor_enrolled_textView);
+        TextView mentorAvailableText = findViewById(R.id.mentor_available_textView);
+
         List<User> enrolledMentees = ListHelpers.where(users, u -> ListHelpers.any(enrolls, e -> e.mentee_id == u.id));
         List<User> enrolledMentors = ListHelpers.where(users, u -> ListHelpers.any(enroll2s, e -> e.mentor_id == u.id));
 
         List<User> availableMentees;
-        if (enrolledMentees.size() <= 6) {
+        if (enrolledMentees.size() < 6) {
             List<Student> availableStudents = ListHelpers.where(students, s -> !ListHelpers.any(enrolls, e -> e.mentee_id == s.student_id) && s.grade == group.description);
+
             for (int i = 0; i < availableStudents.size(); i++) {
                 Student student = availableStudents.get(0);
+
                 List<Enroll> studentEnrolls = ListHelpers.where(enrolls, e -> e.mentee_id == student.student_id);
                 List<Meeting> userMeetings = ListHelpers.where(meetings, m -> ListHelpers.any(studentEnrolls, e -> e.meet_id == m.meet_id));
+
+                for (Meeting userMeeting: userMeetings) {
+                    long diff = Math.abs(userMeeting.date.getTime() - meeting.date.getTime());
+                    long diffDays = diff / (24 * 60 * 60 * 1000);
+
+                    if ((diffDays < 2 && meeting.meet_name == userMeeting.meet_name) || (diffDays == 0 && meeting.date == userMeeting.date)){
+                        availableStudents.remove(i);
+                        i--;
+                        continue;
+                    }
+                }
             }
             availableMentees = ListHelpers.where(users, u -> ListHelpers.any(availableStudents, s -> s.student_id == u.id));
         }
@@ -73,12 +94,25 @@ public class EnrollAdminActivity extends BaseLogoutBackActivity {
         }
 
         List<User> availableMentors;
-        if (enrolledMentors.size() <= 3) {
+        if (enrolledMentors.size() < 3) {
             List<Student> availableStudents = ListHelpers.where(students, s -> !ListHelpers.any(enroll2s, e -> e.mentor_id == s.student_id));
+
             for (int i = 0; i < availableStudents.size(); i++) {
                 Student student = availableStudents.get(0);
+
                 List<Enroll2> studentEnrolls = ListHelpers.where(enroll2s, e -> e.mentor_id == student.student_id);
                 List<Meeting> userMeetings = ListHelpers.where(meetings, m -> ListHelpers.any(studentEnrolls, e -> e.meet_id == m.meet_id));
+
+                for (Meeting userMeeting: userMeetings) {
+                    long diff = Math.abs(userMeeting.date.getTime() - meeting.date.getTime());
+                    long diffDays = diff / (24 * 60 * 60 * 1000);
+
+                    if (diffDays < 2){
+                        availableStudents.remove(i);
+                        i--;
+                        continue;
+                    }
+                }
             }
             availableMentors = ListHelpers.where(users, u -> ListHelpers.any(availableStudents, s -> s.student_id == u.id));
         }
@@ -86,7 +120,23 @@ public class EnrollAdminActivity extends BaseLogoutBackActivity {
             availableMentors = new ArrayList();
         }
 
-        //setupUserAdapter(R.id.enrolled_mentor_recyclerView);
+        if (enrolledMentees.isEmpty()){
+            menteeEnrolledText.setVisibility(View.INVISIBLE);
+        }
+        if (availableMentees.isEmpty()){
+            menteeAvailableText.setVisibility(View.INVISIBLE);
+        }
+        if (enrolledMentors.isEmpty()){
+            mentorEnrolledText.setVisibility(View.INVISIBLE);
+        }
+        if (availableMentors.isEmpty()){
+            mentorAvailableText.setVisibility(View.INVISIBLE);
+        }
+
+        setupUserAdapter(R.id.enrolled_mentee_recyclerView, enrolledMentees, EnrollmentType.MENTEE, false);
+        setupUserAdapter(R.id.enrolled_mentor_recyclerView, enrolledMentors, EnrollmentType.MENTOR, false);
+        setupUserAdapter(R.id.available_mentee_recyclerView, availableMentees, EnrollmentType.MENTEE, true);
+        setupUserAdapter(R.id.available_mentor_recyclerView, availableMentors, EnrollmentType.MENTOR, true);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
--- Phase3/Java/db2/app/src/main/java/com/example/db2/adapters/MeetingAdminEnrollAdapter.java
+++ Phase3/Java/db2/app/src/main/java/com/example/db2/adapters/MeetingAdminEnrollAdapter.java
@@ -1,17 +1,26 @@
 package com.example.db2.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.db2.LandingAdminActivity;
 import com.example.db2.R;
 import com.example.db2.helpers.EnrollmentType;
+import com.example.db2.helpers.QueryExecution;
+import com.example.db2.helpers.UserSession;
+import com.example.db2.models.Enroll;
 import com.example.db2.models.User;
 
 import java.util.List;
@@ -56,11 +65,79 @@ public class MeetingAdminEnrollAdapter extends RecyclerView.Adapter<MeetingAdmin
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull MeetingAdminEnrollAdapter.MeetingViewHolder holder, int position) {
+        ConstraintLayout cl = holder.constraintLayout;
 
+        //get user
+        User user = users.get(position);
+
+        //get text and button UI elements
+        TextView userText = (TextView) cl.getViewById(R.id.user_textView);
+        Button enrollButton = (Button) cl.getViewById(R.id.enroll_button);
+
+        //set text
+        userText.setText(user.name);
+
+        if (!this.isEnrollable){
+            enrollButton.setEnabled(false);
+            enrollButton.setVisibility(View.INVISIBLE);
+        }
+        else {
+            enrollButton.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                @Override
+                public void onClick(View v) {
+                    enrollUser(user);
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
         return users.size();
+    }
+
+    private String getMentTable(){
+        switch (this.enrollmentType){
+            case MENTEE:
+                return "mentee";
+            case MENTOR:
+                return "mentor";
+            case NONE:
+            default:
+                return null;
+        }
+    }
+
+    private String getEnrollTable(){
+        switch (this.enrollmentType){
+            case MENTEE:
+                return "enroll";
+            case MENTOR:
+                return "enroll2";
+            case NONE:
+            default:
+                return null;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void enrollUser(User user){
+        String mentName = getMentTable();
+        String enrollName = getEnrollTable();
+
+        if (mentName == null || enrollName == null){
+            Intent landingAdminIntent = new Intent(context, LandingAdminActivity.class);
+            context.startActivity(landingAdminIntent);
+        }
+
+        String query = String.format("INSERT INTO %ss (%s_id) VALUES (%d)", mentName, mentName, user.id);
+        QueryExecution.executeQuery(query);
+
+        query = String.format("INSERT INTO %s (meet_id, %s_id) VALUES (%d, %d)", enrollName, mentName, MeetingAdminAdapter.targetMeeting.meet_id, user.id);
+        QueryExecution.executeQuery(query);
+
+        ((AppCompatActivity)context).finish();
+        context.startActivity(((AppCompatActivity)context).getIntent());
     }
 }
